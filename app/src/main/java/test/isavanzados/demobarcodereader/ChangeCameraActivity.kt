a50cdcb1639e7a21f344f8112db4ffee880b7a5e
Alberto Araiza <alberto.araiza88@gmail.com>
2020-04-28 17:53:26 -0500

    Adding positioning to HERE activity

---
+++ app/src/main/java/test/isavanzados/demobarcodereader/ChangeCameraActivity.kt
@@ -0,0 +1,12 @@
+package test.isavanzados.demobarcodereader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class changeCameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_camera)
+    }
+}
